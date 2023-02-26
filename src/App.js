e759b1c07a08991afc183a69a8c8feec40872a11
yeonkyoung <keep.ykk@gmail.com>
2023-02-26 16:33:45 +0900

    Feat: CICD 문구 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello! AWS CLI world</h1>
+      <h1>Hello! AWS CICD!</h1>
     </div>
   );
 }
